8dcbc92c05c28eda9762fd03d66db99e5873c960
ToF- <cthibauttof@gmail.com>
2023-03-27 15:42:59 +0200

    added a dummy test

---
+++ src/test/java/org/lags/RentTest.java
@@ -0,0 +1,12 @@
+package org.lags;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RentTest {
+    @Test
+    public void dummy() {
+        assertEquals(4, 2+2);
+    }
+}
